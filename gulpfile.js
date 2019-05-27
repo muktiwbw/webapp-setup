9ff76bf318ecd87f3c7da6427cb7a9667d73f5af
Mukti Wibowo <muktiw7@gmail.com>
2019-05-27 14:32:14 +0700

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,37 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const postcss = require('gulp-postcss')
 const replace = require('gulp-replace')
-const sourcemap = require('gulp-sourcemap')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 
 const srcPath = {
     sass: './app/sass/style.sass',
     js: './app/js/app.bundle.js'
 }
+
+function sassBuild(){
+    return src(srcPath.sass)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(),cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./public/assets/css'))
+}
+
+function jsBuild(){
+    return src(srcPath.js)
+        .pipe(uglify())
+        .pipe(dest('./public/assets/js'))
+}
+
+function cacheBusting(){
+    return src('./public/index.html')
+        .pipe(replace(/cb=\d+/g, 'cb=' + new Date().getTime()))
+        .pipe(dest('./public'))
+}
+
+exports.default = parallel(
+    sassBuild,
+    jsBuild,
+    cacheBusting
+    )
